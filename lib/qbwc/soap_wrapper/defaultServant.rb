cd8ba9d1bd91d12f9f19e65b632f3d279195f52e
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-03-28 13:54:06 -0700

    Remove boilerplate comments from default servant

--- lib/qbwc/soap_wrapper/defaultServant.rb
+++ lib/qbwc/soap_wrapper/defaultServant.rb
@@ -5,57 +5,17 @@ class QBWC::QBWebConnectorSvcSoap
     @client_id = client_id
   end
 
-  # SYNOPSIS
-  #   serverVersion(parameters)
-  #
-  # ARGS
-  #   parameters      ServerVersion - {http://developer.intuit.com/}serverVersion
-  #
-  # RETURNS
-  #   parameters      ServerVersionResponse - {http://developer.intuit.com/}serverVersionResponse
-  #
   def serverVersion(parameters)
-    #p parameters
     QBWC::ServerVersionResponse.new(nil)
   end
 
-  # SYNOPSIS
-  #   clientVersion(parameters)
-  #
-  # ARGS
-  #   parameters      ClientVersion - {http://developer.intuit.com/}clientVersion
-  #
-  # RETURNS
-  #   parameters      ClientVersionResponse - {http://developer.intuit.com/}clientVersionResponse
-  #
   def clientVersion(parameters)
-    #p parameters
     QBWC::ClientVersionResponse.new(nil)
   end
-
-  # SYNOPSIS
-  #   authenticate(parameters)
-  #
-  # ARGS
-  #   parameters      Authenticate - {http://developer.intuit.com/}authenticate
-  #
-  # RETURNS
-  #   parameters      AuthenticateResponse - {http://developer.intuit.com/}authenticateResponse
-  #
-  def authenticate(parameters)
-    #p parameters                               
+  
+  def authenticate(parameters)                              
     QBWC::AuthenticateResponse.new([QBWC.username, QBWC.company_file_path]) #path to company file
   end
-
-  # SYNOPSIS
-  #   sendRequestXML(parameters)
-  #
-  # ARGS
-  #   parameters      SendRequestXML - {http://developer.intuit.com/}sendRequestXML
-  #
-  # RETURNS
-  #   parameters      SendRequestXMLResponse - {http://developer.intuit.com/}sendRequestXMLResponse
-  #
 
   def sendRequestXML(parameters)
     qbwc_session = QBWC.session(@client_id)
@@ -63,58 +23,22 @@ class QBWC::QBWebConnectorSvcSoap
     QBWC::SendRequestXMLResponse.new( next_request ? wrap_in_version(next_request.request) : '') 
   end
 
-  # SYNOPSIS
-  #   receiveResponseXML(parameters)
-  #
-  # ARGS
-  #   parameters      ReceiveResponseXML - {http://developer.intuit.com/}receiveResponseXML
-  #
-  # RETURNS
-  #   parameters      ReceiveResponseXMLResponse - {http://developer.intuit.com/}receiveResponseXMLResponse
-  #
   def receiveResponseXML(response)
     qbwc_session = QBWC.session(@client_id)
     finished = qbwc_session.process_response(response.response)
     QBWC::ReceiveResponseXMLResponse.new(finished ? 100 : 0)
   end
 
-  # SYNOPSIS
-  #   connectionError(parameters)
-  #
-  # ARGS
-  #   parameters      ConnectionError - {http://developer.intuit.com/}connectionError
-  #
-  # RETURNS
-  #   parameters      ConnectionErrorResponse - {http://developer.intuit.com/}connectionErrorResponse
-  #
   def connectionError(parameters)
     #p [parameters]
     raise NotImplementedError.new
   end
 
-  # SYNOPSIS
-  #   getLastError(parameters)
-  #
-  # ARGS
-  #   parameters      GetLastError - {http://developer.intuit.com/}getLastError
-  #
-  # RETURNS
-  #   parameters      GetLastErrorResponse - {http://developer.intuit.com/}getLastErrorResponse
-  #
   def getLastError(parameters)
     #p [parameters]
     QBWC::GetLastErrorResponse.new(nil)
   end
 
-  # SYNOPSIS
-  #   closeConnection(parameters)
-  #
-  # ARGS
-  #   parameters      CloseConnection - {http://developer.intuit.com/}closeConnection
-  #
-  # RETURNS
-  #   parameters      CloseConnectionResponse - {http://developer.intuit.com/}closeConnectionResponse
-  #
   def closeConnection(parameters)
     #p [parameters]
     qbwc_session = QBWC.session(@client_id)
