0ae2b205c78d15568314f94706cb26d2c0fa5b9a
Arthur L. Jones IV <arthur.l.jones@gmail.com>
2013-04-11 10:23:53 -0700

    Bit more cleanup

--- lib/qbwc.rb
+++ lib/qbwc.rb
@@ -1,6 +1,7 @@
 $:.unshift File.dirname(File.expand_path(__FILE__))
 require 'qbwc/version'
 require 'quickbooks'
+require 'soap/rpc/standaloneServer'
 
 module QBWC
 
@@ -13,8 +14,29 @@ module QBWC
 
 class << self
 
-  def create_request(qbxml) 
-    QBWC::Request.new(qbxml)
+  def route_request(servant, request)
+    router = ::SOAP::RPC::Router.new('QBWebConnectorSvcSoap')
+    router.mapping_registry = DefaultMappingRegistry::EncodedRegistry
+    router.literal_mapping_registry = DefaultMappingRegistry::LiteralRegistry
+    connection_data = ::SOAP::StreamHandler::ConnectionData.new
+    servant.soap_actions.each do |action_name|
+      router.add_document_operation(servant,
+        "http://developer.intuit.com/#{action_name}",
+        action_name,
+        [ ["in", "parameters", ["::SOAP::SOAPElement", "http://developer.intuit.com/", "#{action_name}"]],
+          ["out", "parameters", ["::SOAP::SOAPElement", "http://developer.intuit.com/", "#{action_name}Response"]] ],
+        { :request_style =>  :document, :request_use =>  :literal,
+          :response_style => :document, :response_use => :literal,
+          :faults => {} }
+      )
+    end
+
+    connection_data.receive_string = request.raw_post
+    connection_data.receive_contenttype = request.content_type
+    connection_data.soapaction = nil
+
+    router.external_ces = nil 
+    router.route(connection_data)
   end
 
   def on_error=(reaction)
